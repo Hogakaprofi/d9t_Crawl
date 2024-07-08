5590ea22e2efcab4f29e503d0e3a4a44609c342e
Pascal Michailidis <151618550+pemcron@users.noreply.github.com>
2024-07-08 13:44:58 +0200

    modified conver.py

--- Excel_Output/convert.py
+++ Excel_Output/convert.py
@@ -27,22 +27,3 @@ for file_path in file_paths:
             print(f'Fehler beim Verarbeiten der Datei {file_path}: {e}')
     else:
         print(f'Datei {file_path} nicht gefunden.')
-
-#-----------------------------------------------------------------------------------------------------------------------
-# import pandas as pd
-#
-# # Pfad zur JSON-Datei
-# json_file = 'schafferer.json'
-#
-# # Lade die JSON-Daten in einen Pandas DataFrame
-# df = pd.read_json(json_file)
-#
-# # Speichern als CSV-Datei mit UTF-8 Encoding
-# # csv_file = 'ausgabe.csv'
-# # df.to_csv(csv_file, index=False, encoding='utf-8')
-#
-# # Speichern als Excel-Datei mit UTF-8 Encoding
-# excel_file = 'schafferer.xlsx'
-# df.to_excel(excel_file, index=False, engine='openpyxl')  # Hier 'openpyxl' oder 'xlsxwriter' verwenden
-#
-# print(f'Daten erfolgreich als Excel (UTF-8) gespeichert.')
