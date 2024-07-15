cd3cb60baa25e2861208805a63cb006afc651316
Michailidis <Michailidis@hogaka-profi.int>
2024-07-15 16:08:29 +0200

    Top Table works, now only more data categories

--- toptable/toptable/spiders/ttable.py
+++ toptable/toptable/spiders/ttable.py
@@ -6,10 +6,13 @@ class TtableSpider(scrapy.Spider):
     start_urls = ["https://www.top-table.gmbh/"]
 
     seen_urls = set()
+    ignore_urls = {'https://www.top-table.gmbh/Besteck/Steakbesteck'}
 
-    ignore_urls = {'https://www.top-table.gmbh/Kochmesser//'}
+    other_urls = {'https://www.top-table.gmbh/Kochmesser//'}
+    test_urls = {'https://www.top-table.gmbh/Servieren-Transportieren//', 'https://www.top-table.gmbh/Buffet//',
+                 'https://www.top-table.gmbh/Kueche//', 'https://www.top-table.gmbh/Desinfektion-Hygiene//'}
 
-    # Scrape the Main-Page (Startseite)
+    # Scrape the Main-Page (Startseite)c
     def parse(self, response):
         # Get all main categorys of main-page (https://www.grimm-gastrobedarf.de/)
         main_categories = response.css('a.main-navigation-link')
@@ -17,17 +20,54 @@ class TtableSpider(scrapy.Spider):
         for main_category in main_categories:
             # Main Page Urls (Links)
             relative_url = main_category.css('a ::attr(href)').get()
-            if relative_url not in self.ignore_urls:
+            if relative_url in self.other_urls:
+                yield response.follow(relative_url, callback=self.parse_other_category_page)
+            elif relative_url in self.test_urls:
                 yield response.follow(relative_url, callback=self.parse_second_category_page)
             else:
-                yield response.follow(relative_url, callback=self.parse_ignore_category_page)
+                yield response.follow(relative_url, callback=self.parse_test_category_page)
 
     # Second
     def parse_second_category_page(self, response):
-        pass
+        Title_list = []
+        Filter_list = []
 
-    # Scrape the ignore pages
-    def parse_ignore_category_page(self, response):
+        all_categories = response.css('ul.category-navigation.level-1 li.category-navigation-entry')
+        all_filter = response.css('div.filter-multi-select')
+
+        # Categories
+        for category in all_categories:
+            Title_list.append(category.css('a::text').get().strip())
+
+        # Filter
+        for filtered in all_filter:
+            items = filtered.css('label.filter-multi-select-item-label')
+            selection_str = ''
+            for item in items:
+                selection_str += item.css('label.filter-multi-select-item-label::text').get().strip() + ", "
+
+            Filter_list.append(
+                filtered.css('button.filter-panel-item-toggle::text').get().strip() + ': ' + selection_str)
+
+        yield {
+            'Url': response.url,
+            'Title': Title_list,
+            'Filter': Filter_list
+        }
+
+    def parse_test_category_page(self, response):
+        more_categories = response.css('div.cms-element-image')
+
+        for more_category in more_categories:
+            # Main Page Urls (Links)
+            sub_relative_url = more_category.css('a ::attr(href)').get()
+            if sub_relative_url:
+                yield response.follow(sub_relative_url, callback=self.parse_second_category_page)
+            else:
+                pass
+
+    # Scrape the other pages
+    def parse_other_category_page(self, response):
 
         Filter_list = []
         all_filter = response.css('div.filter-multi-select')
@@ -42,6 +82,7 @@ class TtableSpider(scrapy.Spider):
                 filtered.css('button.filter-panel-item-toggle::text').get().strip() + ': ' + selection_str)
 
         yield {
-            'Filter_namen': Filter_list,
             'Url': response.url,
+            'Title': 'Kochmesser',
+            'Filter_namen': Filter_list,
         }
