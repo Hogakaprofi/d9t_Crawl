e9e024a47100cdf1bdc6ad68f13a136c87af2c3b
Michailidis <Michailidis@hogaka-profi.int>
2024-07-15 16:09:13 +0200

    Item added to the tutorial

--- tutorial/tutorial/spiders/tut.py
+++ tutorial/tutorial/spiders/tut.py
@@ -4,23 +4,28 @@
 # To run this spider go into the folder: "..\d9t_Crawl\tutorial> " and run the command: "scrapy crawl tut -O books.json"
 
 import scrapy
-
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
 class TutSpider(scrapy.Spider):
     name = "tut"
     allowed_domains = ["books.toscrape.com"]
     start_urls = ["https://books.toscrape.com"]
 
+    rules = (
+    Rule(LinkExtractor(allow=("https://books.toscrape.com"), deny=( ), ), callback='parse_item', follow=True),)
+
     def parse(self, response):
         books = response.css('article.product_pod')
         for book in books:
             relative_url = book.css('h3 a ::attr(href)').get()
+            title = ''
 
             if 'catalogue/' in relative_url:
                 book_url = 'https://books.toscrape.com/' + relative_url
             else:
                 book_url = 'https://books.toscrape.com/catalogue/' + relative_url
-            yield response.follow(book_url, callback=self.parse_book_page)
+            yield response.follow(book_url, callback=self.parse_book_page, meta={'Title': title})
 
         next_page = response.css('li.next a ::attr(href)').get()
         if next_page is not None:
