cd551a272afb1a330fefd55be56bc4b320e5c757
Michailidis <p.michailidis@hogakaprofi.de>
2024-07-31 10:50:19 +0200

    new run_spiders.sh

--- run_spiders.sh
+++ run_spiders.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Arrays initialisieren
+paths=("/home/user/crawl/d9t_Crawl/grimmgastro" "/home/user/crawl/d9t_Crawl/schafferer/" "/home/user/crawl/d9t_Crawl/toptable" "/home/user/crawl/d9t_Crawl/edgarfuchs" "/home/user/crawl/d9t_Crawl/hinschegastrowelt" "/home/user/crawl/d9t_Crawl/dueGuenther") # Pfade, die durchgegangen werden sollen
+spiders=("grimm" "schaff" "ttable" "edgar" "hinsche" "guenther") # Namen der Spider-Befehle
 
 # Anzahl der Pfade und Spider-Namen überprüfen
 num_paths=${#paths[@]}
@@ -10,6 +12,8 @@ if [ $num_paths -ne $num_spiders ]; then
   echo "Die Anzahl der Pfade und Spider-Namen muss übereinstimmen."
   exit 1
 fi
+
+source home/user/crawl/d9t_Crawl/myenv/bin/activate
 
 # Durch die Pfade und Spider-Namen iterieren
 for ((i=0; i<num_paths; i++)); do
@@ -38,3 +42,10 @@ for ((i=0; i<num_paths; i++)); do
   fi
 done
 
+echo "Alle Spider erfolgreich ausgeführt."
+
+echo ""
+
+python3 /home/user/crawl/d9t_Crawl/Excel_Output/convert.py
+
+echo "Alle json dateien in Excel-Dateien umgewandelt. Bzw. convert.py ausgeführt!"
