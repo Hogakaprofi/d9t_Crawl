03716ad0ea73c23b0863f6588eb94531d42e9890
pmichailidis <79946569+cronhack@users.noreply.github.com>
2024-09-16 09:33:03 +0200

    Update hogaka.py

--- hogakaprofi/hogakaprofi/spiders/hogaka.py
+++ hogakaprofi/hogakaprofi/spiders/hogaka.py
@@ -3,8 +3,34 @@ import scrapy
 
 class HogakaSpider(scrapy.Spider):
     name = "hogaka"
-    allowed_domains = ["shop.hogakaprofi.de"]
-    start_urls = ["https://shop.hogakaprofi.de/"]
+    allowed_domains = ["www.grimm-gastrobedarf.de"]
+    start_urls = ["https://www.grimm-gastrobedarf.de/"]
 
+    seen_urls = set()
+
+    ignore_urls = {'https://www.grimm-gastrobedarf.de/unsere-marken.html',
+                   'https://www.grimm-gastrobedarf.de/sale.html'}
+
+    # Scrape the Main-Page (Startseite)
     def parse(self, response):
-        pass
+        # Get all main categorys of main-page (https://www.grimm-gastrobedarf.de/)
+        all_categories = response.css('a.s360-megamenu__link.s360-megamenu__link--category')
+
+        for category in all_categories:
+            # Main Page Urls (Links)
+            relative_url = category.css('a ::attr(href)').get()
+            if relative_url not in self.ignore_urls:
+                yield response.follow(relative_url, callback=self.parse_second_category_page)
+
+    # Scrape the individual categories (2. Ebene)
+    def parse_second_category_page(self, response):
+        # Get all the individual links of the individual main page categoies
+        second_categories = response.css('a.category-name')
+
+        for second_category in second_categories:
+            # Main Page Urls (Links)
+            sec_relative_url = second_category.css('a ::attr(href)').get()
+            yield {
+                'First lvl': response.url,
+                'second lvl': sec_relative_url
+            }
